e5f67e3474b1cd486fb8518aa2a144540ee718d4
Azraai Baharin <dominoseffect@gmail.com>
2022-01-16 00:10:56 +0800

    enhance cpu and mem usage implementation. Add default erorr handling logic.

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
 'use strict';
 
+// Requires
 const express = require('express');
 const os = require('os');
+const osutils = require('os-utils');
+const date = require('date-and-time');
 
 // Constants
+const date_format = 'YYYY/MM/DD';
+const now = new Date();
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 
-app.set('views', './views')
-app.set('view engine', 'pug')
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index', { 
-    title: 'Hey', 
-    os_info: `${os.platform()} ${os.release()}`,
-    candidate_name: 'Azraai Baharin',
-    date_start_trial: new Date(2021, 12, 13).toDateString(),
-    date_current: new Date().toDateString()
-  })
+  osutils.cpuUsage(function(v) {
+    res.render('index', { 
+      title: 'Hey', 
+      // version 1: Print OS version
+      os_version: `${os.version}`,
+      // version 2: Print trial candidate name, start date and current date
+      candidate_name: 'Azraai Baharin',
+      date_start_trial: date.format(new Date('January 13, 2022'), date_format),
+      date_current: date.format(now, date_format),
+      // version 3: Print CPU and memory usage
+      usage_cpu: `${(v).toFixed(2)} %`,
+      usage_mem: `${(100 - osutils.freememPercentage()).toFixed(2)} %`
+    });
+  });
+});
+
+app.get('/error', (req, res) => {
+  throw new Error('BROKEN');
+});
+
+// basic error handling
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).render('index', {
+    title: 'Hey',
+    error: 'Something broke!'
+  });
 });
 
 app.listen(PORT, HOST);
